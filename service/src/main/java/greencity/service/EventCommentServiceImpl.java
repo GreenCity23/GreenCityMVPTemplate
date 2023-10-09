90fc35ec4e4d3ac443f6c461e0e4a603ed663642
VladyslavSiverskiy <v.siverskiy@gmail.com>
2023-10-09 13:28:17 +0300

    Format code

--- service/src/main/java/greencity/service/EventCommentServiceImpl.java
+++ service/src/main/java/greencity/service/EventCommentServiceImpl.java
@@ -59,7 +59,7 @@ public class EventCommentServiceImpl implements EventCommentService {
         if (addEventCommentDtoRequest.getParentCommentId() != 0) {
             EventComment parentComment = eventCommentRepo.findById(addEventCommentDtoRequest.getParentCommentId())
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.EVENT_COMMENT_NOT_FOUND_BY_ID
-                                                         + addEventCommentDtoRequest.getParentCommentId()));
+                    + addEventCommentDtoRequest.getParentCommentId()));
             if (parentComment.getParentComment() == null) {
                 eventComment.setParentComment(parentComment);
             } else {
